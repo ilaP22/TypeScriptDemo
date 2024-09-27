97a8c047002e4992bb3327b152e7ba7f4bc61ce1
ila <ila.pandey@tothenew.com>
2024-09-27 13:21:46 +0530

    Changes added for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import AddToDo from "./components/addtodo"
-import Navbar from "./components/navbar"
-import Todos from "./components/todos"
-import "./App.css"
+import AddToDo from "./components/addtodo";
+import Navbar from "./components/navbar";
+import Todos from "./components/todos";
+import "./App.css";
 
 const App = () => {
   return (
-   <main>
-      <h1>TASK TRACKER</h1><br/>
+    <main>
+      <h1>TASK TRACKER</h1>
+      <br />
       <Navbar />
       <AddToDo />
       <Todos />
-   </main>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
